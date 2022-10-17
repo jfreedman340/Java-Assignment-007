b390430f7e4a301ef62f5d11b0a506e8b591d12b
jfreedman340 <97777103+jfreedman340@users.noreply.github.com>
2022-10-16 19:02:18 -0700

    I created a program that calculates the GCF.

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,49 @@
+/**
+ * @author Joseph Freedman
+ *
+ * The MyMath class asks the user for two numbers and returns the Greatest Common Factor of them.
+ */
+
 import java.util.Scanner;
 
 public class MyMath {
 
+    /**
+     * @gcf This is the gcf method that takes two parameters and returns the GCF.
+     * @param a is the first integer.
+     * @param b is the second integer.
+     * @return c is the common factor and is returned.
+     */
+    public static int gcf (int a, int b) {
+        int c = 0;
+        while (b != 0) {
+            if (a > b) {
+                c = b;
+                b = a % b;
+                a = c;
+            } else {
+                b = b % a;
+            }
+        }
+        return c;
+    }
+
+    /**
+     * @main This is our main method.
+     *
+     * We are asking the user to input two numbers here and return the GCF.
+     *
+     */
     public static void main(String[] args) {
+        //Ask the user to input two numbers.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first integer: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter the second integer: ");
+        int b = scanner.nextInt();
+
+        //Return the GCF
+        int returnValue =  gcf(a, b);
+        System.out.println("The GCF is " + returnValue);
     }
 }
